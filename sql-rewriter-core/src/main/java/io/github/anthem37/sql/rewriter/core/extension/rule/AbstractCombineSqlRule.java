983c9bd0dc5006e651ee3e67c09e4133fead4bbd
maozhihao301 <maozhihao301@hellobike.com>
2025-11-20 21:03:47 +0800

    refactor(core):重构组合SQL规则抽象类
    - 移除构造函数中对规则列表的直接依赖- 使用 getter 方法替代直接访问规则列表- 简化 match 和 apply 方法中的规则遍历逻辑
    - 删除未使用的导入语句和变量声明

--- sql-rewriter-core/src/main/java/io/github/anthem37/sql/rewriter/core/extension/rule/AbstractCombineSqlRule.java
+++ sql-rewriter-core/src/main/java/io/github/anthem37/sql/rewriter/core/extension/rule/AbstractCombineSqlRule.java
@@ -2,12 +2,8 @@ package io.github.anthem37.sql.rewriter.core.extension.rule;
 
 import io.github.anthem37.sql.rewriter.core.rule.ICombineSqlRule;
 import io.github.anthem37.sql.rewriter.core.rule.ISqlRule;
-import io.github.anthem37.sql.rewriter.core.util.RuleUtils;
 import lombok.Getter;
 import net.sf.jsqlparser.statement.Statement;
-
-import java.util.Collections;
-import java.util.List;
 
 /**
  * 组合SQL规则抽象类
@@ -18,21 +14,15 @@ import java.util.List;
 @Getter
 public abstract class AbstractCombineSqlRule implements ICombineSqlRule {
 
-    private final List<ISqlRule<?>> rules;
-
-    public AbstractCombineSqlRule(List<ISqlRule<?>> rules) {
-        this.rules = Collections.unmodifiableList(RuleUtils.sortSqlRulesByPriority(rules));
-    }
-
     @Override
     public boolean match(Statement statement) {
 
-        return rules.stream().anyMatch(sqlRule -> sqlRule.match(statement));
+        return getRules().stream().anyMatch(sqlRule -> sqlRule.match(statement));
     }
 
     @Override
     public void apply(Statement statement) {
-        for (ISqlRule<?> rule : rules) {
+        for (ISqlRule<?> rule : getRules()) {
             rule.apply(statement);
         }
     }
